0aaaf00b26a7a1ebbcceac1443a6b099a80d931b
Heydaroghlu <heyderovisa777@gmail.com>
2023-10-20 14:01:14 +0400

    Sultan start to Web MVC

---
+++ FurnitureApp/FurnitureApp/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureApp.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FurnitureApp/FurnitureApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FurnitureApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
